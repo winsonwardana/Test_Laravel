ec9f1fd59f47f077fdc0e1eedac82bab74df38f5
Winson <winson91100@gmail.com>
2020-04-22 18:50:32 +0700

    ubah

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Hello, Winson
+                    Hello, Winson, FALi
                 </div>
 
                 <div class="links">
